6ad924960dc76b823326acc2457b950e7b30c942
Nishad112-cyber <nishadguptad@gmail.com>
2026-08-16 07:06:59 +0530

    remove_duplicate

---
+++ Array/Remove_Duplicate.java
@@ -0,0 +1,16 @@
+public class Remove_Duplicate {
+    public static int DuplicateRemove(int nums[]){
+        int k=1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] != nums[i-1]){
+                nums[k] =nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        int nums[] ={1,1,1,2,2,3,3,4,5,5,5};
+        System.out.println(DuplicateRemove(nums));
+    }
+}
