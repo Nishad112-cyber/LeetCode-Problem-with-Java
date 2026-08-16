babc816d90aa14de699dd79e2cbde7f9124b7538
Nishad112-cyber <nishadguptad@gmail.com>
2026-08-16 06:41:13 +0530

    remove_element

---
+++ Array/Remove_Element.java
@@ -0,0 +1,17 @@
+public class Remove_Element {
+    public static int removeElement(int nums[],  int val){
+       int  k=0;
+       for(int i=0; i<nums.length; i++){
+           if(nums[i] != val){
+              nums[k]=nums[i];
+              k++;
+           }
+       }
+       return k;
+
+    }
+    public static void main(String[] args) {
+        int nums[]= {2,3,3,2,2};
+        System.out.print(removeElement(nums, 3)+"");
+    }
+}
