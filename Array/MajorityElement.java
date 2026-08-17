32dae35176ab113cbfdcca4193a27f4a8adc125b
Nishad112-cyber <nishadguptad@gmail.com>
2026-08-17 07:48:57 +0530

    major element

---
+++ Array/MajorityElement.java
@@ -0,0 +1,25 @@
+public class MajorityElement {
+
+    public static int MajorElement(int nums[]){
+       int major=0;
+       int count=0;
+       for(int num : nums){
+            if(count == 0){
+                 major=num;
+            }
+            if(num== major){
+                count++;
+            }else{
+                count--;
+            }
+
+       }
+       return major;
+    }
+    public static void main(String[] args) {
+        int nums[]= {1,1,1,2,2,2,2,1,1,1,1,1};
+     int Ans=   MajorElement(nums);
+        System.out.print(" this is major element : "+Ans);
+    }
+    
+}
